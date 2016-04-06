0fdddbbc86457561898210e172727f1d0fc17bc1
Chris Garvin <chrisgarvin@chrissmacbook2.gateway.sonic.net>
2016-04-06 00:52:23 -0700

    added better ability to get length of post input with emojis within post

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,22 +19,26 @@
 
 $(document).ready(function() {
 
-  if($(".new-user")[0]){
+  //displaying welcome/intro messages based on which page the user is on
+  if ($(".new-user")[0]) {
     message = "Welcome to Charma! Your character limit is equal to the Charma you attain. Increase your Charma as users follow you or like your posts. Like someones post? Throw them some Charma if you have some to spend! Go ahead and get started!"
     setTimeout(typeMessage, 75)
-  } else if($(".welcome")[0]){
+  } else if ($(".welcome")[0]) {
     message = "where your character limit directly correlates to how many people care to read your thoughts";
     setTimeout(typeMessage, 75);
   }
 
   //creating a character count that updates in text field to show user how many characters
   //they have to work with
-  var textInput = $('.new-post')[0];
-  var charLimit = parseInt($('.charma_value')[0].innerText);
-  textInput.addEventListener("keyup", function() {
-    document.getElementsByClassName("character_count")[0].innerText = charLimit - textInput.value.length
-  });
+  if ($('.new-post')[0]) {
+    var textInput = $('.new-post')[0];
+    var charLimit = parseInt($('.charma_value')[0].innerText);
+    textInput.addEventListener("keyup", function() {
+      document.getElementsByClassName("character_count")[0].innerText = charLimit - countEmojis(textInput.value);
+    });
+  }
 
+  //setting up infinite scroll for post_feed as user scrolls to the bottom of the page
   if ($('.pagination').length) {
     $(window).scroll(function() {
       var url = $('.pagination .next_page a').attr('href')
@@ -47,15 +51,18 @@ $(document).ready(function() {
   }
 
 })
+
+//creating a function that mimics typing of a string. sets initial speed to 75ms
+//if character is '.', '!', '?', it changes the delay of the timeout to better mimic typing
 var typingSpeed = 75;
 var counter = 0;
 var typeMessage = function() {
   if (counter <= message.length - 1) {
     $(".typing").show();
     $(".welcome").append(message[counter])
-    if((message[counter] == '.') || (message[counter] == '!')){
+    if ((message[counter] == '.') || (message[counter] == '!')) {
       typingSpeed = 1000;
-    } else if(message[counter] == '?'){
+    } else if (message[counter] == '?') {
       typingSpeed = 500;
     } else {
       typingSpeed = 75;
@@ -67,3 +74,11 @@ var typeMessage = function() {
     $(".typing").hide();
   }
 };
+
+//creating countEmojis function to better calculate string lengths based on javascripts
+//handling of emojis
+var emojiRegex = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+
+function countEmojis(string) {
+  return string.replace(emojiRegex, '_').length;
+}
