96194a37aec30c5c9cc76995d09545e9f2aa922f
Chris Garvin <chrisgarvin@Chriss-MacBook-2.local>
2016-04-07 16:35:34 -0700

    bootstrap layout for mobile orientation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,6 @@ class PostsController < ApplicationController
   def index
     if logged_in?
     @posts = current_user.feed.paginate(page: params[:page], per_page: 10)
-    @leaderboard = Post.order("created_at DESC").limit(5)
     respond_to do |format|
       format.html
       format.js
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def leaderboard
+    Post.order("created_at DESC").limit(5)
+  end
 end
