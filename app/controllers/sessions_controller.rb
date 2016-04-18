46860542960196f255afdbe58c1c5c4f021336f0
Chris Garvin <chrisgarvin@Chriss-MacBook-2.local>
2016-04-17 17:28:12 -0700

    modified new user signup page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     @user = User.find_by_handle(params[:session][:handle].downcase)
     if @user && @user.authenticate(params[:session][:password])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
+    @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       UserMailer.welcome_email(@user).deliver_now
       redirect_to root_path
     else
